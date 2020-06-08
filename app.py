3e7fe6952ad5c07d5aa184bed638ef3f374839ca
jod35 <jodestrevin@gmail.com>
2020-06-08 03:54:16 -0400

    successfully persisted data in a database

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect
+from flask_sqlalchemy import SQLAlchemy
 import requests 
 import os
 from datetime import datetime
 
 app=Flask(__name__)
 
+DB_USER=os.environ.get('DB_USER')
+DB_PASSWORD=os.environ.get('DB_PASSWORD')
 
-#the api key (get yours at fastapi.com)
+app.config['SQLALCHEMY_DATABASE_URI']=f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@localhost/COVID'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+
+db=SQLAlchemy(app)
+
+#the api key (get yours at fastapi.com) mine is an environment variable
 API_KEY=os.environ.get('API_KEY')
 
 url='https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/total'
@@ -19,14 +28,18 @@ req_headers={
 
 previous_data=[]
 
-# @app.route('/')
-# def index():
-# 	data=previous_data
-
-# 	return render_template('index.html',data=data)
 
 
+class Record(db.Model):
+	id=db.Column(db.Integer,primary_key=True)
+	deaths=db.Column(db.Integer,nullable=False)
+	confirmed=db.Column(db.Integer,nullable=False)
+	recovered=db.Column(db.Integer,nullable=False)
+	location=db.Column(db.String(35),nullable=False)
+	last_checked=db.Column(db.Text)
 
+	def __repr__(self):
+		return f'{self.id} record' 
 
 @app.route('/search',methods=['GET','POST'])
 
@@ -46,23 +59,36 @@ def search_cases_by_country():
 		resp_data=response.json()
 
 		data=resp_data['data']
-
-		last_checked=str(data['lastChecked']).replace('T',' at ')
-
-		last_reported=str(data['lastReported']).replace('T',' at ')
-
 		print(data)
+
+
+		new_record=Record(
+			confirmed=data['confirmed'],
+			deaths=data['deaths'],
+			recovered=data['recovered'],
+			location=data['location'],
+			last_checked=data['lastChecked']
+
+			)
+
+		db.session.add(new_record)
+		db.session.commit()
+
 
 		
 
-		context={
-		'data':data,
-		'last_checked':last_checked,
-		'last_reported':last_reported
-		}
+	context={
+	'data':data,
+	}
 
 	return render_template('index.html',**context)
 
+
+#display previous searches
+
+@app.route('/records')
+def return_records():
+	return render_template('records.html')
 
 
 
